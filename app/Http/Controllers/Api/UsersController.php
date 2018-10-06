baf8265ad58a3d680321c45136a7f7cbae692e52
zzb <zhangzhebin@uqian.me>
2018-10-06 09:21:20 +0000

    actived users

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -51,4 +51,9 @@ class UsersController extends Controller
 
         return $this->response->item($user, new UserTransformer());
     }
+
+    public function activedIndex(User $user)
+    {
+        return $this->response->collection($user->getActiveUsers(), new UserTransformer());
+    }
 }
