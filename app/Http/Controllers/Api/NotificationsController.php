0db5203d12816075d8393a67d6af1b68ffe7d724
zzb <zhangzhebin@uqian.me>
2018-10-06 03:42:09 +0000

    notifications read

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -20,4 +20,10 @@ class NotificationsController extends Controller
             'unread_count' => $this->user()->notification_count,
         ]);
     }
+
+    public function read()
+    {
+        $this->user()->markAsRead();
+        return $this->response->noContent();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -61,9 +61,12 @@ $api->version('v1', [
             // 通知列表
             $api->get('user/notifications', 'NotificationsController@index')
                 ->name('api.user.notifications.index');
-            // 通知统计
+            // 未读消息统计
             $api->get('user/notifications/stats', 'NotificationsController@stats')
                 ->name('api.user.notifications.stats');
+            // 标记消息通知为已读
+            $api->patch('user/read/notifications', 'NotificationsController@read')
+                ->name('api.user.notifications.read');
         });
         // 游客可以访问的接口
         // 分类信息
