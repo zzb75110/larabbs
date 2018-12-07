48575d4f712e95917bb0d782d04701590e33f190
vagrant <vagrant@homestead>
2018-12-07 09:08:01 +0000

    weapp update user

--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,8 @@ $api->version('v1', [
             // 编辑登录用户信息
             $api->patch('user', 'UsersController@update')
                 ->name('api.user.update');
+            $api->put('user', 'UsersController@update')
+                ->name('api.user.update');
             // 发布话题
             $api->post('topics', 'TopicsController@store')
                 ->name('api.topics.store');
